550604b35c7625991f3a596b70934a2ac0896fb7
i.alekseev <i.alekseev@iva.ru>
2026-02-12 20:28:54 +0300

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
+  yandexMapsKey: '2d824876-2334-4c72-9a05-b122d6f7ecbe',
+  appName: 'Weather browser',
+};
