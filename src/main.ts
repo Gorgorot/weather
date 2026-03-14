5523567d1b75906723f8fbd71430575a3f06c6c1
i.alekseev <i.alekseev@iva-tech.ru>
2026-03-15 01:14:08 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { AppModule } from './app/app.module';
 import { platformBrowser } from '@angular/platform-browser';
 
-platformBrowser().bootstrapModule(AppModule);
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], });
 
 /*bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));*/
